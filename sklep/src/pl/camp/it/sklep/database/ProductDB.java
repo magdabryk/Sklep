e02e82f04f2b4cb833a5b596e62552301a61fd03
Magda Bryk <brykmagda@gmai.com>
2022-10-30 18:29:40 +0100

    "dodano buyPodukt i payProduct" wq :wq
    i
    i
    -m "dodano metody buyProducy i payProduct"
    :wq
    wq
    q
    w
    
    -m "dodano metody buyProducy i payProduct"
    -m "dodano metody buyProducy i payProduct"

--- sklep/src/pl/camp/it/sklep/database/ProductDB.java
+++ sklep/src/pl/camp/it/sklep/database/ProductDB.java
@@ -5,16 +5,40 @@ import pl.camp.it.sklep.model.Product;
 public class ProductDB {
     private Product[] products = new Product[5];
 
-public ProductDB() {
-    products[0] = new Product(1, "Sword", 250.00, 10);
-    products[1] = new Product(2, "Butterfly knifes", 150.50, 5);
-    products[2] = new Product(3, "Spear", 120.00 , 50);
-    products[3] = new Product(4, "Dagger", 95.00, 9 );
-    products[4] = new Product(5, "Bakwa Dan Do Sword", 500.00 , 3);
+    public ProductDB() {
+        products[0] = new Product(1, "Sword", 250.00, 10);
+        products[1] = new Product(2, "Butterfly knifes", 150.50, 5);
+        products[2] = new Product(3, "Spear", 120.00, 50);
+        products[3] = new Product(4, "Dagger", 95.00, 9);
+        products[4] = new Product(5, "Bakwa Dan Do Sword", 500.00, 3);
 
-}
+    }
 
     public Product[] getProducts() {
         return products;
     }
+
+    public boolean buyProduct(int id, int amount) {
+        for (Product currentProduct : this.products) {
+            if (currentProduct.getId() == id && (currentProduct.getAmount() - amount) >= 0) {
+                int newAmount = currentProduct.getAmount() - amount;
+                currentProduct.setAmount(newAmount);
+                //System.out.println("Do zapłaty " + currentProduct.getPrice() * amount);
+                return true;
+            }
+        }
+        return false;
+    }
+    public void payProduct(int id, int amount) {
+        for (Product currentProduct : this.products) {
+            if(currentProduct.getId() == id) {
+                System.out.println("Do zapłaty " + currentProduct.getPrice() * amount);
+            }
+        }
+    }
+
+
+
+
+
 }
--- sklep/src/pl/camp/it/sklep/engine/Engine.java
+++ sklep/src/pl/camp/it/sklep/engine/Engine.java
@@ -2,37 +2,50 @@ package pl.camp.it.sklep.engine;
 
 import pl.camp.it.sklep.database.ProductDB;
 import pl.camp.it.sklep.gui.GUI;
+import pl.camp.it.sklep.model.Product;
 
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class Engine {
 
-public static void start() {
+    public static void start() {
 
-    ProductDB productDB = new ProductDB();
-    Scanner scanner = new Scanner(System.in);
-    boolean isWorking = true;
+        ProductDB productDB = new ProductDB();
+        Scanner scanner = new Scanner(System.in);
+        boolean isWorking = true;
 
-    while (isWorking) {
-        GUI.showMenu();
-        switch (scanner.nextLine()) {
-            case "1":
-                GUI.poductList(productDB.getProducts());
-                break;
-            case "2":
-                System.out.println("kupowane produktu");
-                break;
-            case "3":
-                System.out.println("uzupłniana magazynu");
-                break;
-            case "4":
-                isWorking = false;
-                break;
-            default:
-                System.out.println("Nie ma takej pozycji w menu. Wybierz jeszcze raz.");
-                break;
+        while (isWorking) {
+            GUI.showMenu();
+            switch (scanner.nextLine()) {
+                case "1":
+                    GUI.poductList(productDB.getProducts());
+                    break;
+                case "2":
+                    System.out.println("Wpisz ID produktu: ");
+                    int buyId = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Wpisz ilość produktu któy chcesz kupić: ");
+                    int buyAmount = Integer.parseInt(scanner.nextLine());
+                    if (productDB.buyProduct(buyId, buyAmount)) {
+                        productDB.payProduct(buyId, buyAmount);
+                    } else {
+                        System.out.println("Nie udało się kupić rządanego produktu wprowadź poprawne ID i ilość sztuk");
+                    }
+
+
+                    break;
+                case "3":
+                    System.out.println("uzupłniana magazynu");
+                    break;
+                case "4":
+                    isWorking = false;
+                    break;
+                default:
+                    System.out.println("Nie ma takej pozycji w menu. Wybierz jeszcze raz.");
+                    break;
+            }
+
         }
-
     }
-}
 }
